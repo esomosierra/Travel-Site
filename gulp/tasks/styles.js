277c35879e416057f5dc6cffccb3f84feb8bfaab
Edmond <edmond.somosierra@gmail.com>
2018-04-14 13:22:06 +0800

    Completed large hero block and intro content block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,23 @@
+var gulp          = require('gulp'),
+    watch         = require('gulp-watch'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer'),
+    cssvars       = require('postcss-simple-vars'),
+    nested        = require('postcss-nested'),
+    cssImport     = require('postcss-import'),
+    mixins        = require('postcss-mixins');
+
+
+/* GENERATES STYLES.CSS MAIN SRC LOCATION TO DESTINATION 'app/temp/styles' using pipe() */
+gulp.task('styles', function() {
+
+    // This needs to "return" because this is asynchrounous call.
+    return gulp.src('./app/assets/css/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) { // ERROR HANDLING: Trigger readable error but gulp is still running.
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles')); 
+
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,35 @@
+var gulp          = require('gulp'),
+    watch         = require('gulp-watch'),
+    browserSync   = require('browser-sync').create();
+
+/* GULP IS WATCHING ANY UPDATES IN INDEX.HTML AND CSS */
+gulp.task('watch', function() {
+
+    // INITIALIZE BROWSER SYNC TO POINT THE LOCATION OF THE BASE FOLDER.
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app",
+        }
+    });
+    
+    // AUTO REFRESH/RELOAD INDEX.HTML AFTER ANY SAVE CHANGES. 
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
+
+    // GULP IS WATCHING CHANGES FROM "./APP/ASSETS/CSS" MAIN STYLES.CSS TO THE TRANSPILED STYLES.CSS LOCATED IN "./APP/TEMP/STYLES/"
+    watch('./app/assets/css/**/*.css', function() {
+        gulp.start('cssInject'); // TASK THAT NEWLY CREATED BELOW AS ARGUMENT HERE
+    });
+
+});
+
+// LIVE INJECT OF ANY SAVE CHANGES OF THE MAIN STYLES.CSS LOCATED IN "./APP/ASSETS/CSS" WITHOUT EVEN AUTO REFRESH/RELOAD
+// MAIN STYLES.CSS WILL THEN TRANSPILED TO STYLES.CSS LOCATED IN "./APP/TEMP/STYLES/"
+
+gulp.task('cssInject', ['styles'], function() { // "['STYLES']" SERVES AS A DEPENDECIES OF 'CSSINJECT'. 
+                                                // BEFORE 'CSSINJECT' RUN, GULP WILL FIRST EXECUTE ['STYLES'] DEPENDENCY.
+    return gulp.src('./app/temp/styles/styles.css') // Compiled CSS
+        .pipe(browserSync.stream());
+});
